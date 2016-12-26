10ed05bee027bf2a3150e48426601d191d6896e9
Peter Turczak <peter@turczak.de>
2016-12-26 12:01:52 +0100

    Added silence "word" as full stop '.'

--- snd/synth.sh
+++ snd/synth.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-word=`echo $1 | tr A-Z a-z`
-echo $word
-say --progress -v Alex -o "$word".aiff "$word"
+word=`echo "$1" | tr A-Z a-z`
+echo -- $word
+if [ "$word" != "." ] ; then
+	say --progress -v Alex -o "$word".aiff -- "$word"
+else
+	sox -n -r 22000 -c 1 "$word".aiff trim 0.0 0.2
+fi
 lame "$word.aiff" "$word.mp3"
